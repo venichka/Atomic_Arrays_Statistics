5eeb8f7599298301450338860aae28c1e012beb9
Nikita <nefedkin@phystech.edu>
2022-11-28 14:26:20 -0500

    g2 function study

---
+++ AtomicArraysStatistics/examples/two_atoms_g2.jl
@@ -0,0 +1,277 @@
+# Two atoms: g2 study
+
+# Activate project
+
+begin
+    if pwd()[end-21:end] == "AtomicArraysStatistics"
+        PATH_ENV = "."
+    else
+        PATH_ENV = "../"
+    end
+    using Pkg
+    Pkg.activate(PATH_ENV)
+end
+
+using LinearAlgebra
+using QuantumOptics
+using PyPlot
+using AtomicArrays
+using Revise
+
+using AtomicArraysStatistics
+
+
+const EMField = field.EMField
+
+
+# System parameters
+const a = 0.18
+const γ = 1.
+const e_dipole = [0, 0, 1.]
+const T = [0:0.05:500;]
+const N = 2
+const Ncenter = 1
+const NMAX = 100
+
+const pos = geometry.chain_dir(a, N; dir="x", pos_0=[-a / 2, 0, 0])
+const Delt = [(i < N) ? 0.0 : 0.5 for i = 1:N]
+const S = SpinCollection(pos, e_dipole; gammas=γ, deltas=Delt)
+const em_inc_function = field.plane
+
+# Define Spin 1/2 operators
+spinbasis = SpinBasis(1//2)
+sx = sigmax(spinbasis)
+sy = sigmay(spinbasis)
+sz = sigmaz(spinbasis)
+sp = sigmap(spinbasis)
+sm = sigmam(spinbasis)
+I_spin = identityoperator(spinbasis)
+
+# Incident field
+E_ampl = 0.2 + 0im
+E_kvec = 2π
+E_pos0 = [0.0, 0.0, 0.0]
+E_polar = [1.0, 0im, 0.0]
+E_angle = [-π / 2, 0.0]
+
+E_inc = EMField(E_ampl, E_kvec, E_angle, E_polar;
+    position_0=E_pos0, waist_radius=0.1)
+
+"""Impinging field"""
+
+x = range(-3.0, 3.0, NMAX)
+y = 0.0
+z = range(-5.0, 5.0, NMAX)
+e_field = Matrix{ComplexF64}(undef, length(x), length(z))
+for i = 1:length(x)
+    for j = 1:length(z)
+        e_field[i, j] = em_inc_function([x[i], y, z[j]], E_inc)[3]
+    end
+end
+
+begin
+    fig_0 = PyPlot.figure(figsize=(7, 4))
+    PyPlot.contourf(x, z, real(e_field)', 30)
+    for p in pos
+        PyPlot.plot(p[1], p[3], "o", color="w", ms=2)
+    end
+    PyPlot.xlabel("x")
+    PyPlot.ylabel("z")
+    PyPlot.colorbar(label="Amplitude")
+    display(fig_0)
+end
+
+
+# Field-spin interaction
+E_vec = [em_inc_function(S.spins[k].position, E_inc) for k = 1:N]
+Om_R = field.rabi(E_vec, S.polarizations)
+
+
+"System Hamiltonian"
+
+Γ, J = AtomicArrays.quantum.JumpOperators(S)
+Jdagger = [dagger(j) for j = J]
+Ω = AtomicArrays.interaction.OmegaMatrix(S)
+H = AtomicArrays.quantum.Hamiltonian(S) - sum(Om_R[j] * J[j] +
+                                                          conj(Om_R[j]) * Jdagger[j]
+                                                          for j = 1:N)
+
+H.data
+
+# eigen(dense(H).data)
+w, v = eigenstates(dense(H))
+
+# Jump operators description
+J_s = AtomicArraysStatistics.jump_op_source_mode(Γ, J)
+
+# Directed-detection jump operators
+
+D = AtomicArraysStatistics.jump_op_direct_detection([10, 10, 10], 0.02^2*pi^2, S, 2π, J)
+D = AtomicArraysStatistics.jump_op_direct_detection(acos(1 / sqrt(3)), π/4, 0.02^2*pi^2, S, 2π, J)
+
+"Dynamics"
+
+# Initial state (Bloch state)
+const phi = 0.
+const theta = pi/1.
+
+# Time evolution
+
+# Quantum: master equation
+sx_master = zeros(Float64, N, length(T))
+sy_master = zeros(Float64, N, length(T))
+sz_master = zeros(Float64, N, length(T))
+
+embed(op::Operator,i) = QuantumOptics.embed(AtomicArrays.quantum.basis(S), i, op)
+
+function fout(t, rho)
+    j = findfirst(isequal(t), T)
+    for i = 1:N
+        sx_master[i, j] = real(expect(embed(sx, i), rho))
+        sy_master[i, j] = real(expect(embed(sy, i), rho))
+        sz_master[i, j] = real(expect(embed(sz, i), rho))
+    end
+    return nothing
+end
+
+Ψ₀ = AtomicArrays.quantum.blochstate(phi,theta,N)
+ρ₀ = Ψ₀⊗dagger(Ψ₀)
+QuantumOptics.timeevolution.master_h(T, ρ₀, H, J; fout=fout, rates=Γ)
+
+sx_copy = copy(sx_master)
+sy_copy = copy(sy_master)
+sz_copy = copy(sz_master)
+
+QuantumOptics.timeevolution.master_h(T, ρ₀, H, J_s; fout=fout)
+
+# Correlation function of the 1st order
+
+tau = [0:0.05:1000;]
+ρ_ss = QuantumOptics.steadystate.eigenvector(H, J; rates=Γ)  # finding the steady-state
+
+corr_1 = QuantumOptics.timecorrelations.correlation(tau, ρ_ss, H, J,
+                                                    Jdagger[1], J[1]; rates=Γ)
+corr_2 = QuantumOptics.timecorrelations.correlation(tau, ρ_ss, H, J,
+                                                    Jdagger[2], J[2]; rates=Γ)
+corr_12 = QuantumOptics.timecorrelations.correlation(tau, ρ_ss, H, J,
+                                                     Jdagger[1] + Jdagger[2],
+                                                     J[1] + J[2]; rates=Γ)
+
+corr_1 = corr_1 .- corr_1[end]
+corr_2 = corr_2 .- corr_2[end]
+corr_12 = corr_12 .- corr_12[end]
+
+omega, spec_1 = QuantumOptics.timecorrelations.correlation2spectrum(tau, corr_1)
+omega, spec_2 = QuantumOptics.timecorrelations.correlation2spectrum(tau, corr_2)
+omega, spec_12 = QuantumOptics.timecorrelations.correlation2spectrum(tau, corr_12)
+
+fig_0 = PyPlot.figure(figsize=(6, 8))
+PyPlot.subplot(211)
+PyPlot.plot(tau, real(corr_1), label="atom 1")
+PyPlot.plot(tau, real(corr_2), label="atom 2")
+PyPlot.plot(tau, real(corr_12), label="atom 1-2")
+PyPlot.xlabel(L"\tau")
+PyPlot.ylabel(L"\langle \sigma^\dag_i \sigma_i \rangle")
+PyPlot.legend()
+
+PyPlot.subplot(212)
+PyPlot.plot(omega, spec_1, label="atom 1")
+PyPlot.plot(omega, spec_2, label="atom 2")
+PyPlot.plot(omega, spec_12, label="atom 1-2")
+PyPlot.xlabel(L"\omega")
+PyPlot.ylabel(L"\mathrm{Spectrum}")
+PyPlot.xlim(-3,3)
+PyPlot.legend()
+
+# g2 function
+
+tau_0 = [0:0.05:100;]
+
+A = sum(Jdagger)
+B = sum(Jdagger) * sum(J)
+C = sum(J)
+
+n_ss = QuantumOptics.expect(B, ρ_ss)
+
+G2 = AtomicArraysStatistics.correlation_3op_1t(tau_0, ρ_ss, H, J, A, B, C;
+                                               rates=Γ)
+
+g2 = G2 / (n_ss * n_ss)
+
+D = AtomicArraysStatistics.jump_op_direct_detection(π/2, 0*π/4, 0.02^2*pi^2, S, 2π, J)
+g2 = AtomicArraysStatistics.coherence_function_g2(tau_0, H, J, D; rates=Γ, rho0=ρ_ss)
+
+fig_00 = PyPlot.figure(figsize=(6, 8))
+PyPlot.subplot(111)
+PyPlot.plot(tau_0, real(g2), label="atom 1")
+PyPlot.xlabel(L"\tau")
+PyPlot.ylabel(L"g^{(2)}(\tau)")
+PyPlot.legend()
+
+
+"g2 depending on angle"
+
+phi_var = range(0, 2π, NMAX)
+theta_var = range(0, π, NMAX ÷ 2)
+
+g2_result = zeros(NMAX)
+
+Threads.@threads for ii in 1:NMAX
+    ϕ = phi_var[ii]
+    D = AtomicArraysStatistics.jump_op_direct_detection(π/2, ϕ, 0.02^2*pi^2, S, 2π, J)
+    g2 = AtomicArraysStatistics.coherence_function_g2(tau_0, H, J, D; rates=Γ, rho0=ρ_ss)
+    g2_result[ii] = real(g2[1])
+end
+
+fig, ax = plt.subplots(1, 1, subplot_kw=Dict("projection" => "polar"), figsize=(5,5))
+ax.plot(phi_var, g2_result)
+# ax.set_rmax(2)
+# ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
+ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
+ax.grid(true)
+ax.set_title(L"g^{(2)}(0)", va="bottom")
+
+"g2 depending on both angles"
+
+g2_result_2D = zeros((NMAX ÷ 2, NMAX))
+
+Threads.@threads for iijj in CartesianIndices((NMAX ÷ 2, NMAX))
+    (ii, jj) = Tuple(iijj)[1], Tuple(iijj)[2]
+    θ = theta_var[ii]
+    ϕ = phi_var[jj]
+    D = AtomicArraysStatistics.jump_op_direct_detection(θ, ϕ, 0.02^2*pi^2, S, 2π, J)
+    g2 = AtomicArraysStatistics.coherence_function_g2(tau_0, H, J, D; rates=Γ, rho0=ρ_ss)
+    g2_result_2D[ii, jj] = real(g2[1])
+    print(ii)
+end
+
+# Plots
+
+fig = PyPlot.figure(figsize=(8, 12))
+PyPlot.subplot(311)
+for i = 1:N
+    PyPlot.plot(T, sx_master[i, :], label="J: atom "*string(i))
+    PyPlot.plot(T, sx_copy[i, :], label="atom "*string(i))
+end
+PyPlot.xlabel("Time")
+PyPlot.ylabel(L"\langle \sigma_x \rangle")
+PyPlot.legend()
+
+PyPlot.subplot(312)
+for i = 1:N
+    PyPlot.plot(T, sy_master[i, :], label="J: atom "*string(i))
+    PyPlot.plot(T, sy_copy[i, :], label="atom "*string(i))
+end
+PyPlot.xlabel("Time")
+PyPlot.ylabel(L"\langle \sigma_y \rangle")
+PyPlot.legend()
+
+PyPlot.subplot(313)
+for i = 1:N
+    PyPlot.plot(T, sz_master[i, :], label="J: atom "*string(i))
+    PyPlot.plot(T, sz_copy[i, :], label="atom "*string(i))
+end
+PyPlot.xlabel("Time")
+PyPlot.ylabel(L"\langle \sigma_z \rangle")
+PyPlot.legend()
+display(fig)
---
+++ AtomicArraysStatistics/src/AtomicArraysStatistics.jl
@@ -0,0 +1,148 @@
+module AtomicArraysStatistics
+
+using LinearAlgebra
+using QuantumOptics
+using AtomicArrays
+
+"""
+    AtomicArraysStatistics.correlation_3op_1t([tspan, ]rho0, H, J, A, B, C; <keyword arguments>)
+Calculate one time correlation values ``⟨A(0)B(\\tau)C(0)⟩``.
+The calculation is done by multiplying the initial density operator
+with ``C \\rho A`` performing a time evolution according to a master equation
+and then calculating the expectation value ``\\mathrm{Tr} \\{B ρ\\}``
+Without the `tspan` argument the points in time are chosen automatically from
+the ode solver and the final time is determined by the steady state termination
+criterion specified in [`steadystate.master`](@ref).
+# Arguments
+* `tspan`: Points of time at which the correlation should be calculated.
+* `rho0`: Initial density operator.
+* `H`: Operator specifying the Hamiltonian.
+* `J`: Vector of jump operators.
+* `A`: Operator at time `t=0`.
+* `B`: Operator at time `t=\\tau`.
+* `C`: Operator at time `t=0`.
+* `rates=ones(N)`: Vector or matrix specifying the coefficients (decay rates)
+        for the jump operators.
+* `Jdagger=dagger.(J)`: Vector containing the hermitian conjugates of the jump
+* `kwargs...`: Further arguments are passed on to the ode solver.
+"""
+function correlation_3op_1t(tspan, rho0::Operator, H::AbstractOperator, J,
+                     A, B, C;
+                     rates=nothing,
+                     Jdagger=dagger.(J),
+                     kwargs...)
+    function fout(t, rho)
+        expect(B, rho)
+    end
+    t,u = timeevolution.master(tspan, C*rho0*A, H, J; rates=rates, Jdagger=Jdagger,
+                        fout=fout, kwargs...)
+    return u
+end
+
+
+"""
+    AtomicArraysStatistics.coherence_function_g2([tspan, ]rho0, H, J, A_op; <keyword arguments>)
+Calculate one time correlation values
+
+``g^{(2)}(\\tau) =
+        \\frac{\\langle A^\\dagger(0)A^\\dagger(\\tau)A(\\tau)A(0)\\rangle}
+        {\\langle A^\\dagger(\\tau)A(\\tau)\\rangle
+         \\langle A^\\dagger(0)A(0)\\rangle}``.
+
+# Arguments
+* `tspan`: Points of time at which the correlation should be calculated.
+* `H`: Operator specifying the Hamiltonian.
+* `J`: Vector of jump operators.
+* `A_op`: Operator at time `t=0`.
+* `rates=ones(N)`: Vector or matrix specifying the coefficients (decay rates)
+        for the jump operators.
+* `rho0=nothing`: Initial density operator, if nothing `rho0 = rho_ss`.
+* `Jdagger=dagger.(J)`: Vector containing the hermitian conjugates of the jump
+* `kwargs...`: Further arguments are passed on to the ode solver.
+"""
+function coherence_function_g2(tspan, H::AbstractOperator, J, A_op;
+                     rates=nothing,
+                     rho0=nothing,
+                     Jdagger=dagger.(J),
+                     kwargs...)
+    function fout(t, rho)
+        expect(dagger(A_op) * A_op, rho)
+    end
+    if isnothing(rho0)
+        rho0 = QuantumOptics.steadystate.eigenvector(H, J; rates=rates)
+        n_ss = [expect(dagger(A_op) * A_op, rho0)]
+    else
+        _, n_ss = timeevolution.master(tspan, rho0, H, J; rates=rates, Jdagger=Jdagger,
+                        fout=fout, kwargs...)
+    end
+    t,u = timeevolution.master(tspan, A_op*rho0*dagger(A_op), H, J; rates=rates, Jdagger=Jdagger,
+                        fout=fout, kwargs...)
+    return u ./ (n_ss[1] * n_ss)
+end
+
+
+"""
+    AtomicArraysStatistics.jump_op_source_mode(Γ, J)
+Calculate the source-mode jump operators
+
+``\\hat{J}_l = \\sqrt{\\lambda_l} \\mathbf{b}_l^T \\hat{\\mathbf{\\Sigma}}``.
+
+# Arguments
+* `Γ`: matrix of decay rates
+* `J`: Vector of common jump operators.
+
+# Return
+* `J_s`: vector of the source-mode jump operators
+"""
+function jump_op_source_mode(Γ, J)
+    N = rank(Γ)
+    λ_g, β_g = eigen(Γ)
+    J_s = [sqrt.(λ_g[l]) * β_g[l,:]' * J for l = 1:N]
+    return J_s
+end
+
+
+"""
+    AtomicArraysStatistics.jump_op_direct_detection(r::Vector, dΩ::Number, S::SpinCollection, J)
+Calculate the direct-detection jump operators
+
+``\\hat{S}(\\theta, \\phi) = \\sqrt{\\gamma D(\\theta, \\phi) d\\Omega} \\sum_{j=1}^N e^{-i k_0 \\hat{r}(\\theta, \\phi) \\cdot \\mathbf{r}_j} \\hat{\\sigma}_j``
+
+``D(\\theta, \\phi) = \\frac{3}{8\\pi} \\left( 1 - \\left[ \\hat{\\mu} \\cdot \\hat{r}(\\theta, \\phi) \\right]^2\\right)``
+
+# Arguments
+* `r`: radius-vector (depends on ``\\theta, \\phi``)
+* `dΩ`: element of solid angle in direction ``r(\\theta, \\phi)``
+* `S`: spin collection
+* `k_0`: wave number, ``k_0 = \\omega_0 / c``, where ``\\omega_0`` is a transition frequency of a atom
+* `J`: Vector of common jump operators.
+
+TODO: take into account different atomic frequencies
+
+# Return
+* `S_op`: vector of the source-mode jump operators
+"""
+function jump_op_direct_detection(r::Vector, dΩ::Number, S::SpinCollection, k_0::Number, J)
+    N = length(S.gammas)
+    μ = S.polarizations[1]
+    γ = S.gammas[1]
+    r_n = r ./ norm(r)
+    D_θϕ = 3.0 / (8.0*pi) * (1 - (μ * r_n')[1]^2)
+    S_op = sqrt.(γ * D_θϕ * dΩ) * sum([exp(-im*k_0*(r_n * S.spins[j].position')[1])*J[j]
+                                       for j = 1:N])
+    return S_op
+end
+
+
+function jump_op_direct_detection(θ::Real, ϕ::Real, dΩ::Number, S::SpinCollection, k_0::Number, J)
+    N = length(S.gammas)
+    μ = S.polarizations[1]
+    γ = S.gammas[1]
+    r_n = [sin.(θ)*cos.(ϕ), sin.(θ)*sin.(ϕ), cos(θ)]
+    D_θϕ = 3.0 / (8.0*pi) * (1 - (μ * r_n')[1]^2)
+    S_op = sqrt.(γ * D_θϕ * dΩ) * sum([exp(-im*k_0*(r_n * S.spins[j].position')[1])*J[j]
+                                       for j = 1:N])
+    return S_op
+end
+
+end # module AtomicArraysStatistics
